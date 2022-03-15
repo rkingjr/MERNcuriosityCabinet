44554d9d5209495295f837f46c5b86f5436f072d
eriksvetlik <svetlik.erik@gmail.com>
2022-03-15 15:11:37 -0500

    updated models for mongoose

--- server/models/User.js
+++ server/models/User.js
@@ -1,62 +1,53 @@
-const { Model, DataType } = require('sequelize');
-const bcrypt = require('bcrypt');
-const sequelize = require('../config/connection');
+const { Schema, model } = require("mongoose");
+const bcrypt = require("bcrypt");
 
-class User extends Model {
-  checkPassword(loginPw) {
-    return bcrypt.compareSync(loginPw, this.password);
-  }
-}
-
-User.init(
-  {
-    id: {
-      type: DataType.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    name: {
-      type: DataType.STRING,
-      allowNull: true,
-    },
-    title: {
-        type: DataType.STRING,
-        allowNull: true,
-        },
-    affiliation: {
-        type: DataType.STRING,
-        allowNull: true,
-        },
-    email: {
-      type: DataType.STRING,
-      allowNull: true,
-      unique: true,
-      validate: {
-        isEmail: true,
-      },
-    },
-    password: {
-      type: DataType.STRING,
-      allowNull: true,
-      validate: {
-        len: [6],
-      },
-    },
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
   },
-  {
-    hooks: {
-      beforeCreate: async (newUserData) => {
-        newUserData.password = await bcrypt.hash(newUserData.password, 10);
-        return newUserData;
-      },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: [/.+@.+\..+/, "Must match an email address!"],
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 5,
+  },
+  title: {
+    type: String,
+    trim: true,
+  },
+  affiliation: {
+    type: String,
+    trim: true,
+  },
+  images: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Image",
     },
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'user',
+  ],
+});
+
+userSchema.pre("save", async function (next) {
+  if (this.isNew || this.isModified("password")) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
   }
-);
+
+  next();
+});
+
+userSchema.methods.isCorrectPassword = async function (password) {
+  return bcrypt.compare(password, this.password);
+};
+
+const User = model("User", userSchema);
 
 module.exports = User;
