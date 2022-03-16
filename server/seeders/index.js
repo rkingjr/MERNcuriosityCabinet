e997177d927ad1d33c7c0bdbca22c7067ccd717e
eriksvetlik <svetlik.erik@gmail.com>
2022-03-16 10:47:58 -0500

    updated models and database name

--- server/seeders/index.js
+++ server/seeders/index.js
@@ -1,25 +1,21 @@
-const db = require('../config/connection');
-const { Collections } = require('../models/Collections');
-const { Comments } = require('../models/Comments');
-const { Image } = require('../models/Image');
-const { User } = require('../models/User');
-const seedCollections = require('./collectionsData.json')
-const seedUser = require('./userData.json');
-const seedImage = require('./imageData.json');
-const seedComments = require('./commentsData.json');
+const db = require("../config/connection");
+const { Collections, Comments, Image, User } = require("../models");
+const seedCollections = require("./collectionsData.json");
+const seedUser = require("./userData.json");
+const seedImage = require("./imageData.json");
+const seedComments = require("./commentsData.json");
 
-db.once('open', async () => {
+db.once("open", async () => {
   try {
     await Collections.deleteMany({});
-    await Collections.create(seedCollections);
     await Comments.deleteMany({});
-    await Comments.create(seedComments);
     await Image.deleteMany({});
-    await Image.create(seedImage);
     await User.deleteMany({});
+    await Collections.create(seedCollections);
+    await Comments.create(seedComments);
+    await Image.create(seedImage);
     await User.create(seedUser);
-
-    console.log('all done!');
+    console.log("all done!");
     process.exit(0);
   } catch (err) {
     throw err;
