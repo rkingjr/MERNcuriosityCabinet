0a788252be5629c24fbb0c27773a1fee84328f4e
Randy King Jr <rkingjr@outlook.com>
2022-03-17 20:52:07 -0500

    update config with db name

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/deep-thoughts',
+  process.env.MONGODB_URI || 'mongodb://localhost/curiositycabinetDB',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
